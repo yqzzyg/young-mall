36274ee39bdddc2d91675beba2fdf85898ab22cd
yqz <452737801@qq.com>
2020-10-30 21:59:15 +0800

    Revert "新增全局异常处理"
    This reverts commit 8bd86c5c

--- young-common/src/main/java/com/young/mall/exception/WebApiException.java
+++ young-common/src/main/java/com/young/mall/exception/WebApiException.java
@@ -7,14 +7,13 @@ import com.young.mall.common.ResErrorCode;
  * @Author: yqz
  * @CreateDate: 2020/10/25 16:11
  */
-public class WebApiException extends RuntimeException {
+public class WebApiException extends RuntimeException{
     private ResErrorCode resErrorCode;
 
     public WebApiException(ResErrorCode resErrorCode) {
         super(resErrorCode.getMsg());
         this.resErrorCode = resErrorCode;
     }
-
     public WebApiException(String msg) {
         super(msg);
     }
