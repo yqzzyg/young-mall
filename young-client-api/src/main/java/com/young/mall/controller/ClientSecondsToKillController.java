267ae0dc333392909f81c582709e0ff11be3ff6d
yqz <452737801@qq.com>
2021-02-01 15:14:05 +0800

    增加秒杀相关代码model

---
+++ young-client-api/src/main/java/com/young/mall/controller/ClientSecondsToKillController.java
@@ -0,0 +1,16 @@
+package com.young.mall.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description: 秒杀抢购
+ * @Author: yqz
+ * @CreateDate: 2021/2/1 12:21
+ */
+@Api(tags = "ClientSecondsToKillController", description = "秒杀相关")
+@RestController
+@RequestMapping("/client/seckill")
+public class ClientSecondsToKillController {
+}
