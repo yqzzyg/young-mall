b7bd86080cdb41519a99a7794c07e36546d8fc6e
yqz <452737801@qq.com>
2021-02-07 09:44:27 +0800

    秒杀后台“秒杀时间段选择”增加状态标识

--- young-db/src/main/java/com/young/db/pojo/SeckillPromotionSessionDetail.java
+++ young-db/src/main/java/com/young/db/pojo/SeckillPromotionSessionDetail.java
@@ -3,8 +3,6 @@ package com.young.db.pojo;
 import com.young.db.entity.YoungSeckillPromotionSession;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
  * @Description: 包含商品数量的场次信息
@@ -16,4 +14,7 @@ public class SeckillPromotionSessionDetail extends YoungSeckillPromotionSession 
 
     @ApiModelProperty("商品数量")
     private Integer productCount;
+
+    @ApiModelProperty("开启状态")
+    private Integer status;
 }
